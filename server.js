e3a26ed1b360bfd10c28d84ea62a7c20950b12d4
osamakhalid123 <osamakhalid797@gmail.com>
2022-08-02 17:10:47 +0200

    port

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/api/pages', pageRoute);
 app.use('/api/users', userRoute);
 app.use('/api/assets', assetRoute);
 
-const PORT = 8080;
-app.listen(process.env.PORT || 8080, () => {
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
   console.log(`server is runnin g at http://localhost:${PORT}/api/`);
 });
